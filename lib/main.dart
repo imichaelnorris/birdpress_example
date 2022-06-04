b7f21b51dd9efa68a1a703cd965d37ed8d12cdee
Michael Norris <imichaelnorris@gmail.com>
2022-06-03 17:53:31 -0700

    first commit. repo works roughly

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,9 @@
 import 'package:flutter/material.dart';
 
+import 'package:birdpress/birdpress.dart';
+import 'package:url_strategy/url_strategy.dart';
 
 void main() {
+  setPathUrlStrategy();
+  runApp(BirdPress());
 }
